86ea4cd4f13d4f132951c10d533bdc95e6bc618a
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-08 11:10:58 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score="33ab"
+// let score=null
+let score=false
+// console.log(typeof score);
+// console.log(typeof (score));
+// node 01_basics/03_conversionOperation.js
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN(not a number)
+//true=>1 false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=>true 0=>false 
+// ""=>false 
+// "palak"=>true 
+
+let someNumber=null
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
