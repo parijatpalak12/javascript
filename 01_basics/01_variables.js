53869f9dd0dbd34704175d2aa1683a92a1efb19b
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-08 09:22:31 +0000

    data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=173838
+let accountEmail="palak@gmail.com"
+var accountPassword="2355"
+accountCity="jaipur"
+let accountState;
+
+// let accountId=65767 cannot declare again
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);
+// tc39
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log(3+3);
+console.log("palak");
+
+let name="palak"
+let age=21
+let isLoggedIn=true
+let state=null
+
+// null=>standalone value 
+// undefined => value is not assigned
+// symbol=>unique
+
+console.log(typeof "palakk")
+console.log(typeof undefined)
+console.log(typeof null)
