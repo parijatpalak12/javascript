4bd218b3410c081fb6d3566faf347175dadeb4ac
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-18 15:15:37 +0000

    truthy and falsy values

---
+++ 04_control-flow/truthy.js
@@ -0,0 +1,11 @@
+const useremail="palak@kesh.ai"
+if(useremail){
+    console.log("got user email")
+}
+else{
+    console.log("didnt got user email")
+}
+// falsy values
+// false, 0, -0, BigInt 0n, " ", null, undefined, NaN
+// TRUTHY VALUES
+// "0",'false' TRUE, " ",[],{}, function(){},
