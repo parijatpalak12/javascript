b3824a8dbb71b7275e831266b0042caeb3a83b68
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-18 15:14:13 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+function one(){
+    const username="parijat"
+    function two(){
+        const website="youtube"
+        // console.log(username)
+    }
+    two()
+    // console.log(website)
+}
+one()
+
+
+addone(5)
+function addone(num){
+    return num+1;
+}
+
+
+const addtwo=function(num){
+    return num+2;
+}
+addtwo(5)
