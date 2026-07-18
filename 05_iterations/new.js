34793aea91fefe755b69d8f822df188841d99ecd
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-18 15:50:27 +0000

    while and do while

---
+++ 05_iterations/new.js
@@ -0,0 +1,14 @@
+let myarray=["mumbai","delhi","bangalore"]
+let arr=0
+while(arr<myarray.length){
+    // console.log(myarray[arr]);
+    arr+=1    
+}
+
+let score=11
+do{
+    console.log(score);
+    score+=1
+    
+}
+while(score<=10)
