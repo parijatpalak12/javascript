fb8032664f0b7a3e194dd5b4859065187cf6bef8
parijatpalak12 <parijatpalakkeshri@gmail.com>
2026-07-19 07:45:05 +0000

    filter

---
+++ 05_iterations/five.js
@@ -0,0 +1,15 @@
+const coding=["c","ruby","java","python",'c++']
+const values=coding.forEach((item)=>{
+    // console.log(item);    
+})
+
+// const nums=[1,2,3,4,5,6,7,8,9,10]
+// const mynums=nums.filter((num)=>num>4)
+// console.log(mynums);
+// or
+const nums=[1,2,3,4,5,6,7,8,9,10]
+const mynums=nums.filter((num)=>{
+    return num>4
+})
+console.log(mynums);
+
